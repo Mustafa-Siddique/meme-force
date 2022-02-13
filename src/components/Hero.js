7e557e483a70f3cf2f3ce6952008c936ee662dc4
Mustafa-Siddique <66897095+Mustafa-Siddique@users.noreply.github.com>
2022-02-13 08:44:33 +0530

    nav-responsive

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,11 +7,19 @@ import {MdSecurity} from 'react-icons/md'
 
 export default function Hero() {
 
-    const heroSocials = useRef(null)
+    const heroTele = useRef(null)
+    const heroBsc = useRef(null)
+    const heroEth = useRef(null)
     
     useEffect(() => {
-        gsap.from(heroSocials.current, {rotateZ:25, opacity:0, display:"none", delay:5.5})
-        gsap.to(heroSocials.current,{display: "inline-block", duration: 0.5, rotate: 0})
+        gsap.from(heroTele.current, {rotateZ:25, opacity:0, display:"none"})
+        gsap.to(heroTele.current,{display: "inline-block", duration: 0.5, delay:5.5})
+
+        gsap.from(heroBsc.current, {rotateZ:25, opacity:0, display:"none"})
+        gsap.to(heroBsc.current,{display: "inline-block", duration: 0.5, delay:5.5})
+
+        gsap.from(heroEth.current, {rotateZ:25, opacity:0, display:"none"})
+        gsap.to(heroEth.current,{display: "inline-block", duration: 0.5, rotate: 0, delay:5.5, autoAlpha:1})
     },[])
     
 
@@ -25,9 +33,9 @@ export default function Hero() {
             <p>MemeForce is the next big cross-chain launchpad project, available on the Binance SmartChain and Ethereum Mainnet.</p>
         </div>
         <div className="hero-socials">
-            <a href="https://t.me/MemeForceOfficial"><span ref={heroSocials}>Telegram</span> <FaTelegramPlane/></a>
-            <a href="http://bscscan.com/"><span ref={heroSocials}>BSC Scan</span> <AiFillSecurityScan/></a>
-            <a href="https://etherscan.io/"><span ref={heroSocials}>Ether Scan</span> <MdSecurity/></a>
+            <a href="https://t.me/MemeForceOfficial"><span ref={heroTele}>Telegram</span> <FaTelegramPlane/></a>
+            <a href="http://bscscan.com/"><span ref={heroBsc}>BSC Scan</span> <AiFillSecurityScan/></a>
+            <a href="https://etherscan.io/"><span ref={heroEth}>Ether Scan</span> <MdSecurity/></a>
         </div>
     </div>
   )
