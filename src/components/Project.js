b9a4ee7da88d4d8b46bd07b04e0005bc1e4eb219
Mustafa <mstfasid@gmail.com>
2022-02-19 18:47:33 +0530

    static-swap

--- src/components/Project.js
+++ src/components/Project.js
@@ -121,7 +121,7 @@ export default function Project() {
             </div>
             <Navlaunch />
             <div className="container-fluid position-relative">
-                <div className="row mt-5">
+                <div className="row cont mt-5">
                     <div className="col-lg-5">
                         <div className="project-head">
                             <div className="logo">
@@ -139,17 +139,42 @@ export default function Project() {
                                 </div>
                                 <div className="sale-stat">&bull; Open</div>
                                 <div className="chain">BNB</div>
-                                <p className='fs-6' style={{color:"#6c757d"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, optio doloribus reiciendis velit cupiditate sapiente eius animi vel ullam quo? Accusamus rem, magnam aut non corrupti nobis porro ducimus accusantium?</p>
-                                <div className="action-button">
-                                    <a href="/" className="btn rounded-pill py-2 px-4 text-light" style={{backgroundColor:"#F0B90B"}}>Approve</a>
-                                    <a href="/" className="btn rounded-pill py-2 px-4 ms-2 text-light" style={{backgroundColor:"#F0B90B"}}>Join Pool</a>
-                                </div>
+                                <p className='fs-6' style={{ color: "#6c757d" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, optio doloribus reiciendis velit cupiditate sapiente eius animi vel ullam quo? Accusamus rem, magnam aut non corrupti nobis porro ducimus accusantium?</p>
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-5">
                         <div className="token-desc">
-
+                            <div className="row">
+                                <div className="col swap">
+                                    <div className='position-relative' style={{maxWidth:"255px"}}>
+                                        <p className='m-0'>Your balance: <span style={{fontWeight:"500"}}>0.0000 BNB</span></p>
+                                        <button style={{backgroundColor:"#660033", border:"none", borderRadius:"5px", color:"#fff", position:"absolute", right:"5px", marginTop:"-5px"}}>Max</button><br />
+                                        <input type="text" id='swapFrom'/>
+                                    </div>
+                                    <div className='position-relative mt-4' style={{maxWidth:"255px"}}>
+                                        <input type="text" id='swapTo'/>
+                                    </div>
+                                    <button className="btn mt-4" style={{backgroundColor:"#660033", border:"none", borderRadius:"5px", color:"#fff", width:"100%"}}>Buy</button>
+                                    <button className="btn mt-1 desabled text-muted" style={{backgroundColor:"#660033", border:"none", borderRadius:"5px", width:"100%"}}>Claim</button>
+                                </div>
+                                <div className="col">
+                                    <p>Remaning allocation: <span>0.0000</span></p>
+                                    <p className='m-0'>Reserved Token: <br /><span style={{fontWeight:"500"}}>0.0000 MEMEFORCE</span></p>
+                                    <br />
+                                    <p className='m-0'>Rate: <br /><span style={{fontWeight:"500"}}>1 BNB ≈ 1,111,111 MEMEFORCE</span></p>
+                                    <br />
+                                    <p>Swap Progress:</p>
+                                    <div className="raised">
+                                        <div className="progress" style={{ height: "10px" }}>
+                                            <div className="progress-bar progress-bar-striped" role="progressbar" style={{ width: "75%" }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                        <div className='justify-content-between d-flex'>
+                                            <p>Funds Raised:</p><p> 245 BNB</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
