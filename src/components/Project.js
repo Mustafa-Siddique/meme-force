17f668c4c7f243b5940faa3e8c89efcbb9a50e6a
Nikkrana28 <60419615+Nikkrana28@users.noreply.github.com>
2022-03-30 01:19:52 +0530

    logo fixed

--- src/components/Project.js
+++ src/components/Project.js
@@ -240,7 +240,7 @@ export default function Project() {
             <div className="project-head">
               <div className="logo">
                 <img
-                  src="https://images.unsplash.com/photo-1602934445884-da0fa1c9d3b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGxvZ298ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                  src={presalestaring ? presalestaring.logo : "#"}
                   alt=""
                 />
                 <img id="project-logo-anim" src={logoBg} alt="" />
